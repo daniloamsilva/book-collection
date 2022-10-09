7fd3eb643ac57d49cd9bff60b19149dadd8d21db
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-10-09 19:28:34 -0300

    Validation

---
+++ src/books/books.error.ts
@@ -0,0 +1,6 @@
+export const errors = {
+  TITLE_IS_STRING: 'O título deve ser um texto.',
+  TITLE_IS_NOT_EMPTY: 'O título é obrigatório.',
+  PAGES_IS_NUMBER: 'O número de páginas deve ser um número.',
+  PAGES_IS_NOT_EMPTY: 'O número de páginas é obrigatório.',
+};
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,4 +1,12 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+import { errors } from '../books.error';
+
 export class CreateBookDto {
+  @IsString({ message: errors.TITLE_IS_STRING })
+  @IsNotEmpty({ message: errors.TITLE_IS_NOT_EMPTY })
   title: string;
+
+  @IsNumber({}, { message: errors.PAGES_IS_NUMBER })
+  @IsNotEmpty({ message: errors.PAGES_IS_NOT_EMPTY })
   pages: number;
 }
