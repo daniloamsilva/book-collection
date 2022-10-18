bd294207a15ff1540878d73029bc1824e333b787
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-10-17 21:44:30 -0300

    Auth tutorial implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BooksModule],
+  imports: [ConfigModule.forRoot(), BooksModule, AuthModule, UsersModule],
+  controllers: [AppController],
 })
 export class AppModule {}
